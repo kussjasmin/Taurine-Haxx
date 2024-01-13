95115b61a48d3b91aeec5a84c3e488091e457902
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2024-01-13 08:42:52 -0500

    Update fileproviderctl_internal.m

--- fileproviderctl_internal.m
+++ fileproviderctl_internal.m
@@ -1,17 +1,27 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
+#import <IOKit/IOKitLib.h>
 #import "TSUtil.h"
 #import "SBFApplication.h"
 
 BOOL isJailbroken(void);
+char* getBootArgs(void);
 
 int main (int __unused argc, char* argv[]) {
+    char* bootArgs = getBootArgs();
+    if (bootArgs) {
+        if (strstr(bootArgs, "no_untether") != NULL) {
+            printf("no_untether boot arg found");
+            return -1;
+        }
+    }
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSString* TaurinePath = [NSString stringWithFormat:@"%@/Taurine", [[SBFApplication alloc] initWithApplicationBundleIdentifier: @"org.coolstar.taurine"].bundleURL.path];
     if (!isJailbroken() && [fileManager fileExistsAtPath: @"/var/mobile/.untether"]) {
         sleep(45);
         spawnRoot(TaurinePath, @[@"jailbreak"]);
+        [UIPasteboard generalPasteboard].string = @"Untethered code execution!!";
     }
 }
 
@@ -25,4 +35,11 @@ BOOL isJailbroken(void) {
         }
     }
     return false;
+}
+
+char* getBootArgs(void) {
+    io_registry_entry_t entry = IORegistryEntryFromPath(MACH_PORT_NULL, "IODeviceTree:/options");    
+    CFStringRef cfNvramVar = IORegistryEntryCreateCFProperty(entry, CFSTR("boot-args"), kCFAllocatorDefault, 0);
+    char* nvramVar = (char*)CFStringGetCStringPtr(cfNvramVar, kCFStringEncodingUTF8);
+    return nvramVar;
 }
